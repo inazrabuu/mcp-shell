d72d658f41ee696047ccb8debc5476e7c19ef2e1
inazrabuu <inazrabuu@gmail.com>
2025-05-09 06:50:51 +0700

    added new functions

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
 import requests
 
+base_url = 'http://localhost:8000'
+
 def call_mcp_tools(tool_name, input_data):
-  url = f"http://localhost:8000/tools/{tool_name}/call"
+  url = f"{base_url}/tools/{tool_name}/call"
   response = requests.post(url, json=input_data)
 
   if response.status_code == 200:
     return response.json()
   else:
     raise Exception(f"MCP error: {response.status_code}: {response.text}")
+  
+def available_tools():
+  response = requests.get(f"{base_url}/tools")
+
+  return response.json()
